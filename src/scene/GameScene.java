8f0b7e0312c37a7782a551590b3216f3fa052410
Kantapon Natepisanwanich <earthneat@gmail.com>
2021-05-19 23:31:59 +0700

    add gui.game package

--- src/scene/GameScene.java
+++ src/scene/GameScene.java
@@ -1,15 +1,2 @@
 package scene;
 
-import javafx.geometry.Pos;
-import javafx.scene.layout.VBox;
-
-public class GameScene extends VBox{
-	public GameScene() {
-		int width = 1024, height = 768;
-		this.setMinWidth(width);
-		this.setMaxWidth(height);
-		this.setMinHeight(height);
-		this.setMaxHeight(height);
-	  
-	}
-}
