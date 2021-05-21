18bd2b10cfaa8db67a4b88b3ee0b0bc987428b32
Kantapon Natepisanwanich <earthneat@gmail.com>
2021-05-22 01:30:41 +0700

    update startscreen and inpututility

--- src/gui/start/StartScreen.java
+++ src/gui/start/StartScreen.java
@@ -13,26 +13,27 @@ public class StartScreen extends StackPane {
 
 	public StartScreen() {
 		this.setAlignment(Pos.BOTTOM_CENTER);
-		int width=1024,height=768;
+		int width = 1024, height = 768;
 		this.setMinWidth(width);
 		this.setMaxWidth(height);
 		this.setMinHeight(height);
 		this.setMaxHeight(height);
-		Canvas canvas = new Canvas(1024,768);
+		Canvas canvas = new Canvas(1024, 768);
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		drawBackground(gc);
 		this.getChildren().add(canvas);
 		ButtonStart button = new ButtonStart();
 		this.getChildren().add(button);
 	}
+
 	private void drawBackground(GraphicsContext gc) {
-		gc.drawImage(ImageHolder.getInstance().startBackground, 0, 0,1024,768);
-		gc.drawImage(ImageHolder.getInstance().lightRedPole1D.get(6),100, 57,124,180);
+		gc.drawImage(ImageHolder.getInstance().startBackground, 0, 0, 1024, 768);
+		gc.drawImage(ImageHolder.getInstance().lightRedPole1D.get(6), 100, 57, 124, 180);
 		gc.setFont(FontHolder.getInstance().gameFont_200);
 		gc.setFill(Color.BLUE);
 		gc.setStroke(Color.WHITE);
-		gc.fillText("Overload",306,190 );
-	//	gc.setLineWidth(2);
-		gc.strokeText("Overload",306,190 );
+		gc.fillText("Overload", 306, 190);
+		// gc.setLineWidth(2);
+		gc.strokeText("Overload", 306, 190);
 	}
 }
--- src/input/InputUtility.java
+++ src/input/InputUtility.java
@@ -3,68 +3,80 @@ package input;
 import java.util.ArrayList;
 
 public class InputUtility {
-	private static final int recieve=1,get=2;
-	private static int[] atSameAction = {0,0,0,0,0,0,0,0,0,0,0};
+	private static int[] atSameAction = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	private static boolean atSameActionEntered = false;
-	private static boolean stopDice=false;
-	private static boolean choosePoleZero=false,choosePoleOne=false;
-	private static int numberOfRings=-1,numberDice;
-	
+	private static boolean stopDice = false;
+	private static boolean choosePoleZero = false, choosePoleOne = false;
+	private static int numberOfRings = -1, numberDice;
+
 	public static int getNumberDice() {
 		return numberDice;
 	}
+
 	public static void setNumberDice(int numberDice) {
 		InputUtility.numberDice = numberDice;
 	}
-	public static void setAtSameAction(int idx,int act) {
-		atSameAction[idx]=act;
+
+	public static void setAtSameAction(int idx, int act) {
+		atSameAction[idx] = act;
 	}
+
 	public static void setAtSameActionEnter(boolean act) {
-		atSameActionEntered=act;
+		atSameActionEntered = act;
 	}
-	
+
 	public static int[] getAtSameAction() {
 		return atSameAction;
 	}
+
 	public static boolean isAtSameActionEntered() {
 		return atSameActionEntered;
 	}
+
 	public static void afterAtSameAction() {
-		for(int i=0;i<=9;i++) {
-			atSameAction[i]=0;
+		for (int i = 0; i <= 9; i++) {
+			atSameAction[i] = 0;
 		}
 		setAtSameActionEnter(false);
 	}
+
 	public static boolean isStopDice() {
 		return stopDice;
 	}
+
 	public static void setStopDice(boolean StopDice) {
 		InputUtility.stopDice = StopDice;
 	}
-	
+
 	public static void afterStopDice() {
 		setStopDice(false);
 	}
-	
+
 	public static boolean isChoosePoleZero() {
 		return choosePoleZero;
 	}
+
 	public static void setChoosePoleZero(boolean choosePoleZero) {
 		InputUtility.choosePoleZero = choosePoleZero;
 	}
+
 	public static boolean isChoosePoleOne() {
 		return choosePoleOne;
 	}
+
 	public static void setChoosePoleOne(boolean choosePoleOne) {
 		InputUtility.choosePoleOne = choosePoleOne;
 	}
+
 	public static void afterChoosePole() {
 		setChoosePoleOne(false);
 		setChoosePoleZero(false);
 	}
+
 	public static int getNumberOfRings() {
 		return numberOfRings;
 	}
+
 	public static void setNumberOfRings(int numberOfRings) {
 		InputUtility.numberOfRings = numberOfRings;
 	}
