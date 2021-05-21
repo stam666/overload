2a831934b74bedeb52fa0e87f31da57f6b687a1d
stam666 <70497397+stam666@users.noreply.github.com>
2021-05-22 02:16:33 +0700

    calibrate

--- src/gui/board/BoardPane.java
+++ src/gui/board/BoardPane.java
@@ -31,8 +31,8 @@ public class BoardPane extends Pane {
 			List<Tile> lane = new ArrayList<Tile>(length);
 			for (int j = 0; j < length; j++) {
 				Tile tile = new Tile((player * 2 - i) * 10);
-				tile.setX((i * tile.getWidth() * 105 / 450) + (j * tile.getWidth() * 345 / 450));
-				tile.setY(0 + i * tile.getHeight());
+				tile.setX((i * tile.getWidth() * 105 / 450) + ((j+1.7) * tile.getWidth() * 345 / 450));
+				tile.setY(30 + i * tile.getHeight());
 				lane.add(tile);
 				RenderableHolder.getInstance().add(tile);
 			}
