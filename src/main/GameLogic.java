c6542cd677dff92a2b9b088a97877d8779f6bdb7
Kantapon Natepisanwanich <earthneat@gmail.com>
2021-05-22 23:03:59 +0700

    add constants state

--- src/main/GameLogic.java
+++ src/main/GameLogic.java
@@ -14,18 +14,18 @@ public class GameLogic {
 	private static ArrayList<Player> players;
 	private static Scanner kb = new Scanner(System.in);
 	private static final int scoreToWin = 35;
-	static int nowState = 1;
+	static int nowState = Constants.stateSelectPole;
 	private static int stageLength = 18, nowNumberSubPlayer, oldStage, nowStage;
 	protected static SubPlayer nowSubPlayer;
 	protected static ArrayList<SubPlayer> atSame = new ArrayList<SubPlayer>();
 	private static int turnofPlayer = 0, nextAction;
 	private static Boolean update = false, updateSame = false, change = false, changeSame = false;
 	private static double factor = 1024.0D / 18.0D / 450.0D;
-	
+
 	public static int getNextAction() {
 		return nextAction;
 	}
-	
+
 	public static Boolean getUpdate() {
 		return update;
 	}
@@ -64,7 +64,8 @@ public class GameLogic {
 
 	public static void initialize(ArrayList<Player> player) {
 		players = player;
-		nowState = 1;turnofPlayer=0;
+		nowState = Constants.stateSelectPole;
+		turnofPlayer = 0;
 	}
 
 	public static void CheckAtEnd() {
@@ -84,21 +85,21 @@ public class GameLogic {
 		// Random ran = new Random();
 		// int dice = ran.nextInt(8)+1;
 		if (players.get(idx).getSub().get(0).getStage() == 0) {
-			nowState = 3;
+			nowState = Constants.stateAskRings;
 			nowNumberSubPlayer = 0;
 			nowSubPlayer = players.get(turnofPlayer).getSub().get(0);
 		} else if (players.get(idx).getSub().get(1).getStage() == 0) {
-			nowState = 3;
+			nowState = Constants.stateAskRings;
 			nowNumberSubPlayer = 1;
 			nowSubPlayer = players.get(turnofPlayer).getSub().get(1);
 		} else {
 			// RenderChoosePole
 			change = true;
 			nextAction = Constants.choosePole;
-			nowState = 2;
+			nowState = Constants.statePlayerChoosePole;
 		}
 	}
-	
+
 	public static void CheckPassedPole() {
 		for (int i = 0; i < players.size(); i++) {
 			for (int j = 0; j <= 1; j++) {
@@ -139,121 +140,124 @@ public class GameLogic {
 		}
 		atSame.remove(idx);
 	}
+
 	private static void setUpdate() {
 		update = false;
 		updateSame = false;
 		change = false;
 		changeSame = false;
 	}
+
 	private static Boolean allAtSameIsZero() {
-		for(SubPlayer e : atSame) {
-			if(e.getRings()!=0) {
+		for (SubPlayer e : atSame) {
+			if (e.getRings() != 0) {
 				return false;
 			}
 		}
 		return true;
 	}
+
 	public static void updateLogic() {
 		setUpdate();
-		if (nowState == 1) {
+		if (nowState == Constants.stateSelectPole) {
 			choosePole();
-		} else if (nowState == 2) {
+		} else if (nowState == Constants.statePlayerChoosePole) {
 			if (InputUtility.isChoosePoleZero()) {
 				nowNumberSubPlayer = 0;
-				nowState = 5;
+				nowState = Constants.stateGetDice;
 				nowSubPlayer = players.get(turnofPlayer).getSub().get(0);
 				change = true;
 				nextAction = Constants.rollDice;
 				InputUtility.afterChoosePole();
 			} else if (InputUtility.isChoosePoleOne()) {
 				nowNumberSubPlayer = 1;
-				nowState = 5;
+				nowState = Constants.stateGetDice;
 				nowSubPlayer = players.get(turnofPlayer).getSub().get(1);
 				change = true;
 				nextAction = Constants.rollDice;
 				InputUtility.afterChoosePole();
 			}
-		} else if (nowState == 3) {
+		} else if (nowState == Constants.stateAskRings) {
 			// RenderAskNumberOfRings
 			change = true;
 			nextAction = Constants.chooseNumber;
 
-			nowState = 4;
-		} else if (nowState == 4) {
+			nowState = Constants.stateGetRings;
+		} else if (nowState == Constants.stateGetRings) {
 			if (InputUtility.getNumberOfRings() != -1) {
 				setRingsToSubPlayer(InputUtility.getNumberOfRings());
-				
+
 				// renderRolldice
 				change = true;
 				nextAction = Constants.rollDice;
-				nowState = 5;
+				nowState = Constants.stateGetDice;
 				InputUtility.setNumberOfRings(-1);
 			}
-		} else if (nowState == 5) {
+		} else if (nowState == Constants.stateGetDice) {
 			update = true;
 			if (InputUtility.isStopDice()) {
 				update = true;
 				int dice = InputUtility.getNumberDice();
 				oldStage = nowSubPlayer.getStage();
 				nowStage = nowSubPlayer.addStage(dice);
-				nowState = 6;
+				nowState = Constants.stateRenderWalk;
 				InputUtility.setStopDice(false);
 			}
-		} else if (nowState == 6) {
+		} else if (nowState == Constants.stateRenderWalk) {
 			// renderwalk
-			
-			nowState = 7;
+
+			nowState = Constants.stateCheckPass;
 //			if(render fin ) {
 //				nowState=7;
 //			}
-		} else if (nowState == 7) {
+		} else if (nowState == Constants.stateCheckPass) {
 			atSame.clear();
 			CheckPassedPole();
-			nowState = 8;
-		} else if (nowState == 8) {
+			nowState = Constants.stateRenderPass;
+		} else if (nowState == Constants.stateRenderPass) {
 			// renderadd pass pole
 //			if(render fin ) {
 //			nowState=9;
 //			}
-			nowState = 9;
-		} else if (nowState == 9) {
-			if (atSame.isEmpty()|| (nowSubPlayer.getRings()==0 && allAtSameIsZero())) {
-				nowState = 12;
+			nowState = Constants.stateCheckAtSamePlace;
+		} else if (nowState == Constants.stateCheckAtSamePlace) {
+			if (atSame.isEmpty() || (nowSubPlayer.getRings() == 0 && allAtSameIsZero())) {
+				nowState = Constants.stateEndTurn;
 			} else {
 				// render
 				changeSame = true;
-				nowState = 10;
+				nowState = Constants.stateAtSameAction;
 			}
-		} else if (nowState == 10) {
-			if (atSame.isEmpty() || (nowSubPlayer.getRings()==0 && allAtSameIsZero()) ) {
-				nowState = 12;
+		} else if (nowState == Constants.stateAtSameAction) {
+			if (atSame.isEmpty() || (nowSubPlayer.getRings() == 0 && allAtSameIsZero())) {
+				nowState = Constants.stateEndTurn;
 			} else if (InputUtility.isAtSameActionEntered()) {
 				for (int i = 0; i < atSame.size(); i++) {
 					if (InputUtility.getAtSameAction()[i] != 0) {
 						atSameAction(i, InputUtility.getAtSameAction()[i]);
-						nowState = 11;
+						nowState = Constants.stateRenderAtSame;
 						updateSame = true;
 						InputUtility.afterAtSameAction();
 						break;
 					}
 				}
 			}
-		} else if (nowState == 11) {
+		} else if (nowState == Constants.stateRenderAtSame) {
 //			if(render fin ) {
 //			nowState=12;
 //			}
-			if (atSame.isEmpty() || (nowSubPlayer.getRings()==0 && allAtSameIsZero()) ) {
-				nowState = 12;
+			if (atSame.isEmpty() || (nowSubPlayer.getRings() == 0 && allAtSameIsZero())) {
+				nowState = Constants.stateEndTurn;
 			} else {
-				nowState = 1;
+				nowState = Constants.stateAtSameAction;
 			}
-		} else if (nowState == 12) {
+		} else if (nowState == Constants.stateEndTurn) {
 			CheckEndgame();
 			CheckAtEnd();
 			// System.out.println("hi");
 			turnofPlayer += 1;
 			turnofPlayer %= 4;
-			nowState = 1;
+			nowState = Constants.stateSelectPole;
 		}
 	}
 
@@ -273,7 +277,5 @@ public class GameLogic {
 	public static int getStageLength() {
 		return stageLength;
 	}
-	
-	
-	
+
 }
--- src/sharedObject/Constants.java
+++ src/sharedObject/Constants.java
@@ -7,4 +7,16 @@ public class Constants {
 	public static final int choosePole=2,chooseNumber=3,atSame=4,noAction=0;
 	public static final int get=1,give=2;
 	public static final String[] colorList = {"BLUE", "GREEN", "PINK", "RED", "YELLOW"};
+	public static final int stateSelectPole = 1;
+	public static final int statePlayerChoosePole = 2;
+	public static final int stateAskRings = 3;
+	public static final int stateGetRings = 4;
+	public static final int stateGetDice = 5;
+	public static final int stateRenderWalk = 6;
+	public static final int stateCheckPass = 7;
+	public static final int stateRenderPass = 8;
+	public static final int stateCheckAtSamePlace = 9;
+	public static final int stateAtSameAction = 10;
+	public static final int stateRenderAtSame = 11;
+	public static final int stateEndTurn = 12;
 }
