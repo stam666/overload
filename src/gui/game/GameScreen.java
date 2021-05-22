b96f0702adc656be6b443b80a557176e6a794a52
Kantapon Natepisanwanich <earthneat@gmail.com>
2021-05-23 01:39:53 +0700

    Update GameScreen.java

--- src/gui/game/GameScreen.java
+++ src/gui/game/GameScreen.java
@@ -3,15 +3,13 @@ package gui.game;
 import java.util.ArrayList;
 
 import gui.action.ActionPane;
+import gui.board.BoardCanvas;
 import gui.board.BoardPane;
 import gui.score.ScoreboardPane;
 import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.VBox;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.util.Duration;
 import main.GameLogic;
 import main.logic;
 import player.Player;
@@ -20,19 +18,17 @@ import sharedObject.Constants;
 import sharedObject.IRenderable;
 import sharedObject.RenderableHolder;
 
-public class GameScreen extends VBox {
+public class GameScreen extends VBox{
 	private ScoreboardPane score;
 	private ActionPane action;
-	private BoardPane board;
+	private BoardCanvas boardCanvas;
 	private Canvas gameCanvas;
 	private GraphicsContext gamegc;
 	private ArrayList<Player> players;
 	private AnimationTimer gameLoop;
-	private Boolean updateSame = false, update = false, changeSame = false, change = false;
+	private Boolean updateSame=false,update=false,changeSame=false,change=false;
 	private int nextAction;
-
 	public GameScreen() {
-		
 		players = new ArrayList<Player>();
 		players.add(new Player("BLUE"));
 		players.add(new Player("GREEN"));
@@ -46,66 +42,49 @@ public class GameScreen extends VBox {
 		this.setMaxHeight(height);
 		this.score = new ScoreboardPane(this.players);
 		this.action = new ActionPane();
-		this.board = new BoardPane(players.size(), GameLogic.getStageLength());
-		this.gameCanvas = new Canvas(1024, 256);
-		gamegc = this.gameCanvas.getGraphicsContext2D();
-		board.getChildren().add(gameCanvas);
-		int count = 0;
-		for (Player e : players) {
-			e.getSub().get(0).setLane(this.board.getBoard().get(count));
-			count++;
-			e.getSub().get(1).setLane(this.board.getBoard().get(count));
-			count++;
-		}
-		this.getChildren().addAll(this.score, this.action, this.board);
+		this.boardCanvas = new BoardCanvas();
+		this.getChildren().addAll(this.score, this.action, this.boardCanvas);
 		this.loop();
 	}
-
-	private void loop() {
-		this.gameLoop = new AnimationTimer() {
-			public void handle(long now) {
-				
-				GameLogic.updateLogic();
-				RenderableHolder.getInstance().update();
-
-				for (IRenderable entity : RenderableHolder.getInstance().getEntities()) {
-					entity.draw(gamegc);
-				}
-
-				updateSame = GameLogic.getUpdateSame();
-				update = GameLogic.getUpdate();
-				changeSame = GameLogic.getChangeSame();
-				change = GameLogic.getChange();
-				nextAction = GameLogic.getNextAction();
-				if (change) {
-					// System.out.println(nextAction);
-					GameScreen.this.update(true, nextAction);
-
-				} else if (changeSame) {
-					GameScreen.this.updateAtSame(true, Constants.atSame, GameLogic.getAtSame(),
-							GameLogic.getNowSubPlayer().getRings() > 0);
-				} else if (update) {
-					GameScreen.this.update(false, -1);
-				} else if (updateSame) {
-					GameScreen.this.updateAtSame(false, Constants.atSame, GameLogic.getAtSame(),
-							GameLogic.getNowSubPlayer().getRings() > 0);
-				}
-
-				/*
-				 * for(Player e:players) { SubPlayer a=e.getSub().get(0),b=e.getSub().get(1);
-				 * System.out.printf("%d  %d %d\n",GameLogic.getNowState(),a.getStage(),b.
-				 * getStage()); } System.out.println(GameScreen.this.action.getMode());
-				 */
-
-			}
-		};
-		this.gameLoop.start();
-	}
-
+	 private void loop() {
+	    this.gameLoop = new AnimationTimer() {
+		        public void handle(long now) {
+		        	boardCanvas.clearScreen();
+					GameLogic.updateLogic();
+					RenderableHolder.getInstance().update();
+					boardCanvas.drawGameObject();
+					
+					updateSame=GameLogic.getUpdateSame();
+					update=GameLogic.getUpdate();
+					changeSame=GameLogic.getChangeSame();
+					change=GameLogic.getChange();
+					nextAction=GameLogic.getNextAction();
+					if(change) {
+						//System.out.println(nextAction);
+						GameScreen.this.update(true,nextAction);
+						
+					}else if(changeSame) {
+						GameScreen.this.updateAtSame(true,Constants.atSame,GameLogic.getAtSame(),GameLogic.getNowSubPlayer().getRings()>0);
+					}else if(update) {
+						GameScreen.this.update(false,-1);
+					}else if(updateSame) {
+						GameScreen.this.updateAtSame(false,Constants.atSame,GameLogic.getAtSame(),GameLogic.getNowSubPlayer().getRings()>0);
+					}
+		
+					/*for(Player e:players) {
+						SubPlayer a=e.getSub().get(0),b=e.getSub().get(1);
+						System.out.printf("%d  %d %d\n",GameLogic.getNowState(),a.getStage(),b.getStage());
+					}
+					System.out.println(GameScreen.this.action.getMode());*/
+		          
+		        }
+		      };
+		    this.gameLoop.start();
+		  }
 	private void update(Boolean changeAction, int nextAction) {
 		GameScreen.this.score.update();
 		if (changeAction) {
-
+			
 			GameScreen.this.action.changeMode(nextAction);
 		} else {
 			GameScreen.this.action.update();
