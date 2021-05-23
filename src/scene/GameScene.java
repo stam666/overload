b453aff4eaaac97ae28bb50f0a893318cdb8c1e5
Kantapon Natepisanwanich <earthneat@gmail.com>
2021-05-23 19:09:56 +0700

    update scene

--- src/scene/GameScene.java
+++ src/scene/GameScene.java
@@ -16,7 +16,6 @@ import player.SubPlayer;
 import sharedObject.Constants;
 
 public class GameScene extends Scene {
-	private GameScreen game;
 
 	private GameScene(Parent root) {
 		super(root);
@@ -24,59 +23,6 @@ public class GameScene extends Scene {
 
 	public GameScene() {
 		this((Parent) new GameScreen());
-		//initialize();
-		
 	
 	}
-	/*private GameScreen1 gameScreen;
-	public static  boolean updateSame=false,update=false,changeSame=false;
-	public static boolean change=false;
-	static int a=0,b=0,changeNo;
-	private ArrayList<Player> players;
-
-	public GameScene(Parent root) {
-		super(root);
-	}
-
-	public GameScene() {
-		this((Parent) new GameScreen1(players));
-		initialize();
-	}
-
-	private void initialize() {
-		GameScreen1 gameScreen = new GameScreen1(players);
-		logic.initialize(players);
-		
-		setRoot((Parent) (this.gameScreen = gameScreen));
-		
-		AnimationTimer animation = new AnimationTimer() {
-			public void handle(long now) {
-				//g.update(true,3);
-				updateSame=false;
-				update=false;
-				changeSame=false;
-				change=false;
-				logic.updateLogic();
-				if(change) {
-					gameScreen.update(true,changeNo);
-				}else if(changeSame) {
-					gameScreen.updateAtSame(true,Constants.atSame,logic.atSame,logic.nowSubPlayer.getRings()>0);
-				}else if(update) {
-					gameScreen.update(false,-1);
-				}else if(updateSame) {
-					gameScreen.updateAtSame(false,Constants.atSame,logic.atSame,logic.nowSubPlayer.getRings()>0);
-				}
-	
-				for(Player e:players) {
-					SubPlayer a=e.getSub().get(0),b=e.getSub().get(1);
-					System.out.printf("%d  %d %d %d %d\n",logic.nowState,a.getRings(),a.getStage(),b.getRings(),b.getStage());
-				}
-				System.out.println("///////////////////////////////////////////////////");
-				
-			}
-		};
-		animation.start();
-		
-	}*/
-
 }
