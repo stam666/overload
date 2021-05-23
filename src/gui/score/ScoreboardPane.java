f3c72a51adc281717fd221a3471e09f46655aec7
Kantapon Natepisanwanich <earthneat@gmail.com>
2021-05-23 18:01:03 +0700

    update scoreboard

--- src/gui/score/ScoreboardPane.java
+++ src/gui/score/ScoreboardPane.java
@@ -9,12 +9,14 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import player.Player;
+
 public class ScoreboardPane extends HBox {
 	private ArrayList<PlayerPane> playerPanes;
 	private ArrayList<Player> players;
+
 	public ScoreboardPane(ArrayList<Player> players) {
-		this.players=players;
-		int width=1024,height=256;
+		this.players = players;
+		int width = 1024, height = 256;
 		this.setMinWidth(width);
 		this.setMaxWidth(height);
 		this.setMinHeight(height);
@@ -22,30 +24,21 @@ public class ScoreboardPane extends HBox {
 		this.setAlignment(Pos.CENTER);
 		this.setSpacing(10);
 		this.playerPanes = new ArrayList<PlayerPane>();
-		int count=0;
-		for(Player e :this.players) {
-			playerPanes.add(new PlayerPane(count,e));
+		int count = 0;
+		for (Player e : this.players) {
+			playerPanes.add(new PlayerPane(count, e));
 			count++;
 		}
-		for(VBox e :playerPanes) {
+		for (VBox e : playerPanes) {
 			this.getChildren().add(e);
 		}
 	}
+
 	public void update() {
-		int count=0;
-		for(Player e :this.players) {
+		int count = 0;
+		for (Player e : this.players) {
 			playerPanes.get(count).update();
 			count++;
 		}
-		/*this.getChildren().clear();
-		this.playerPanes.clear();
-		int count=0;
-		for(Player e :players) {
-			playerPanes.add(new PlayerPane(count,e));
-			count++;
-		}
-		for(VBox e :playerPanes) {
-			this.getChildren().add(e);
-		}*/
 	}
 }
